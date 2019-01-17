0e5a05ba7de9b6da9a878bf523e8ac5908a56744
asus <the.superenz@gmail.com>
2019-01-17 16:29:53 +0800

    Running Add/View Venue

---
+++ resources/views/rooms/addroom.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Add Rooms</h1>
+    {!! Form::open(['action' => 'VenuesController@store', 'method' => 'POST' ,
+    'enctype' => 'multipart/form-data']) !!}
+    <div class="form-group">
+        {{Form::label('venueName', 'Name')}}
+        {{Form::text('venueName', '', ['class' => 'form-control', 'placeholder'
+        => 'VenueName'])}}
+    </div>
+
+
+    {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+    {!! ! Form::close() !!}
+@endsection
