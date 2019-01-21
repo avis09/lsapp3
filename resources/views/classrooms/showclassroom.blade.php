3f19e2d143f5db360036da87f02f54b9c6a16974
asus <the.superenz@gmail.com>
2019-01-21 14:10:42 +0800

    Running Add/View Venue

--- resources/views/classrooms/showclassroom.blade.php
+++ resources/views/classrooms/showclassroom.blade.php
@@ -2,17 +2,19 @@
 
 @section('content')
     <a href="/classrooms" class="btn btn-default">Go Back</a>
-    <h1>{{$classrooms->RoomFloor}}</h1>
     <div>
-        {!!$classrooms->RoomNumber!!}
+        {!!$classrooms->RoomFloor!!}
     </div>
+    <h1>
+        {!!$classrooms->RoomNumber!!}
+    </h1>
     <div>
         {!!$classrooms->venueID!!}
     </div>
     <div>
         {!!$classrooms->venueName!!}
     </div>
-    <a href="/classrooms/{{$classrooms->id}}/edit" class="btn btn-default">Edit</a>
+    <a href="/classrooms/{{$classrooms->id}}edit" class="btn btn-default">Edit</a>
 
 
 @endsection
