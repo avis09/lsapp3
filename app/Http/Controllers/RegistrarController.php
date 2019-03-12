02519451fe395b15746e3ab7a6ccfdf96371d019
jananzelsantos <anz.zel17@gmail.com>
2019-03-12 23:06:14 +0800

    tweaks

--- app/Http/Controllers/RegistrarController.php
+++ app/Http/Controllers/RegistrarController.php
@@ -10,15 +10,15 @@ class RegistrarController extends Controller
 {
     public function showDashboard(){
 
-//schedules count
-        $count = DB::table('schedules')
+//total number of schedules count
+        $countAllSchedules = DB::table('schedules')
             ->join('venue', 'venue.venueID', '=', 'schedules.venueID')
             ->select('schedules.scheduleID')
             ->where('venue.venueTypeID', '=', '1')
             ->count();
 
         return view('pages.registrar.dashboard')
-            ->with('count', $count);
+            ->with('countAllSchedules', $countAllSchedules);
 
     }
 
--- resources/views/pages/registrar/dashboard.blade.php
+++ resources/views/pages/registrar/dashboard.blade.php
@@ -27,34 +27,34 @@
           </ul>
         </div>
         <div class="row">
-          <div class="col-md-6 col-lg-3">
+          <div class="col-md-6 col-lg-4">
             <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
               <div class="info">
                 <h4>Schedules Count</h4>
-                <p><b>{{$count}}</b></p>
+                <p><b>{{$countAllSchedules}}</b></p>
               </div>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3">
+          <div class="col-md-6 col-lg-4">
                 <div class="widget-small info coloured-icon"><i class="icon fa fa-users fa-3x"></i>
               <div class="info">
-                <h4>Active Users</h4>
+                <h4>Total number of rooms</h4>
                 <p><b>25</b></p>
               </div>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3">
+          <div class="col-md-6 col-lg-4">
                 <div class="widget-small warning coloured-icon"><i class="icon fa fa-users fa-3x"></i>
               <div class="info">
-                <h4>Inactive Users</h4>
+                <h4>Unavailable Rooms</h4>
                 <p><b>10</b></p>
               </div>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3">
+          <div class="col-md-6 col-lg-4">
                 <div class="widget-small danger coloured-icon"><i class="icon fa fa-users fa-3x"></i>
               <div class="info">
-                <h4>Archived Users</h4>
+                <h4>Active Rooms</h4>
                 <p><b>500</b></p>
               </div>
             </div>
