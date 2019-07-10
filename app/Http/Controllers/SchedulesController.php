6692b0961ba373c9b6dee8d69dfa3f4b890aaf3f
Reinn Macario <reinnmacario@gmail.com>
2019-07-11 01:33:25 +0800

    fix date in calendar  availability if  rejected or cancelled

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -361,8 +361,8 @@ class SchedulesController extends Controller
     {
         $check_schedule = Schedule::where('venueID', $request->id)->where('date', $request->date)->get();
         if(count($check_schedule) > 0){
-            $schedules = Venue::distinct()->select('schedules.date','time.timeStartTime','time.timeEndTime')
-            ->selectSub('IF((SELECT timeID from schedules where timeID = time.timeID AND venueID = '.$request->id.' AND date = "'.$request->date.'"),1,0)', 'check')
+            $schedules = Venue::distinct()->select('schedules.date', 'time.timeStartTime', 'time.timeEndTime')
+            ->selectSub('IF((SELECT timeID from schedules where timeID = time.timeID AND venueID = '.$request->id.' AND date = "'.$request->date.'" AND statusID NOT IN(3,4,6)),1,0)', 'check')
             ->join('venuetype', 'venue.venueTypeID', 'venuetype.venueTypeID')
             ->leftJoin('time', 'venueType.venueTypeID', 'time.venueTypeID')
             ->rightJoin('schedules', 'venue.venueID', 'schedules.venueID')
