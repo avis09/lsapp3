19f0d936c72902aeae216b18746c339c4ba36118
Reinn Macario <reinnmacario@gmail.com>
2019-07-15 00:21:42 +0800

    avoid invalid image, added 404 and 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page not found - 404</title>
+
+    <style>
+    body {
+        background-color: #02542d;
+        color: white;
+        font-family: "Lato", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    }
+
+    .error-message {
+        position: relative;
+        text-align: center;
+    }
+
+    .error-message h1 {
+        font-size: 80px;
+        margin-bottom: 0px;
+    }
+
+    .error-message p {
+        font-size: 30px;
+    }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="error-message">
+            <h1>500</h1>
+            <p>Internal server error.</p>
+        </div>
+    </div>
+</body>
+</html>
